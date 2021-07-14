a8ef4af368ea12f3bb101f0c570baddafc93a306
SGI-Github <66118384+SGI-Github@users.noreply.github.com>
2021-07-15 07:44:37 +0900

    Section 6 - MiCard - Lesson 41 How to Use Container Widgets - Completed. Next Lesson is 42 - How to use Column & Row Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyApp()
   );
 }
+
+//StateLess Widget
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text("Hello"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
